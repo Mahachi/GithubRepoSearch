f38f81dc36b12a510a6a90cc1969bef8acdb357d
Ngoni Mahachi <ngonexmahachi@gmail.com>
2019-08-27 19:57:49 +0200

    Added a menu

--- app/src/main/java/com/ngonimahachi/githubreposearch/MainActivity.java
+++ app/src/main/java/com/ngonimahachi/githubreposearch/MainActivity.java
@@ -2,10 +2,14 @@ package com.ngonimahachi.githubreposearch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText mSearchBoxEditText;
@@ -20,7 +24,23 @@ public class MainActivity extends AppCompatActivity {
         mSearchBoxEditText = findViewById(R.id.et_search_box);
         mUrlDisplayTextView = findViewById(R.id.tv_url_display);
         mSearchResultsTextView = findViewById(R.id.tv_github_search_results_json);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_search) {
+            Context context = MainActivity.this;
+            String textToShow = "Search clicked";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
