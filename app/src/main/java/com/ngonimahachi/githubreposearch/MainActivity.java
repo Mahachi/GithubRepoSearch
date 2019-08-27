3493339c8cd5037dd27e57be88156098f70df496
Ngoni Mahachi <ngonexmahachi@gmail.com>
2019-08-27 21:08:10 +0200

    polishing up

--- app/src/main/java/com/ngonimahachi/githubreposearch/MainActivity.java
+++ app/src/main/java/com/ngonimahachi/githubreposearch/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -16,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText mSearchBoxEditText;
     private TextView mUrlDisplayTextView;
     private Button mSearchResultsTextView;
+    private ProgressBar mLoadingIndicator;
+    private TextView mErrorMessageDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         mSearchBoxEditText = findViewById(R.id.et_search_box);
         mUrlDisplayTextView = findViewById(R.id.tv_url_display);
         mSearchResultsTextView = findViewById(R.id.tv_github_search_results_json);
+        mErrorMessageDisplay = findViewById(R.id.tv_error_message_display);
+        mLoadingIndicator = findViewById(R.id.pb_loading_indicator);
     }
 
     // Created a method called makeGithubSearchQuery
@@ -49,6 +56,35 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+    //  Created a method called showJsonDataView to show the data and hide the error
+    /**
+     * This method will make the View for the JSON data visible and
+     * hide the error message.
+     * <p>
+     * Since it is okay to redundantly set the visibility of a View, we don't
+     * need to check whether each view is currently visible or invisible.
+     */
+    private void showJsonDataView() {
+        // First, make sure the error is invisible
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+        // Then, make sure the JSON data is visible
+        mSearchResultsTextView.setVisibility(View.VISIBLE);
+    }
+
+     //Created a method called showErrorMessage to show the error and hide the data
+    /**
+     * This method will make the error message visible and hide the JSON
+     * View.
+     * <p>
+     * Since it is okay to redundantly set the visibility of a View, we don't
+     * need to check whether each view is currently visible or invisible.
+     */
+    private void showErrorMessage() {
+        // First, hide the currently visible data
+        mSearchResultsTextView.setVisibility(View.INVISIBLE);
+        // Then, show the error
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    }
 
         @Override
         public boolean onCreateOptionsMenu(Menu menu) {
@@ -66,27 +102,34 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         }
-        return super.onOptionsItemSelected(item);
-     //Created a class called GithubQueryTask that extends AsyncTask<URL, Void, String>
+         return super.onOptionsItemSelected(item);
+
+    //Created a class called GithubQueryTask that extends AsyncTask<URL, Void, String>
     public class GithubQueryTask extends AsyncTask<URL, Void, String> {
 
-        //  Overrided the doInBackground method to perform the query. Return the results. (Hint: You've already written the code to perform the query)
-        @Override
-        protected String doInBackground(URL... params) {
-            URL searchUrl = params[0];
-            String githubSearchResults = null;
-            try {
-                githubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return githubSearchResults;
-        }
+         //  Overrided the doInBackground method to perform the query. Return the results. (Hint: You've already written the code to perform the query)
+         @Override
+         protected String doInBackground(URL... params) {
+             URL searchUrl = params[0];
+             String githubSearchResults = null;
+             try {
+                 githubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+             } catch (IOException e) {
+                 e.printStackTrace();
+             }
+             return githubSearchResults;
+         }
 
-        //  Overrided onPostExecute to display the results in the TextView
-        @Override
-        protected void onPostExecute(String githubSearchResults) {
-            if (githubSearchResults != null && !githubSearchResults.equals("")) {
-                mSearchResultsTextView.setText(githubSearchResults);
-            }
+         //  Override onPostExecute to display the results in the TextView
+         @Override
+         protected void onPostExecute(String githubSearchResults) {
+             mLoadingIndicator.setVisibility(View.INVISIBLE);
+             if (githubSearchResults != null && !githubSearchResults.equals("")) {
+                 showJsonDataView();
+             }else{
+                 showErrorMessage();
+             }
+
+         }
+     }
 }
